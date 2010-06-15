13c38f37dceb2cdf023e7cc50a6a325905ea6f77
Brian Goslinga <gosli008@morris.umn.edu>
2010-06-15 12:43:42 -0500

    Start of redesign

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gml "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+(defproject gml "0.0.1-SNAPSHOT"
+  :description "An easier way to make simple GUIs"
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]]
-  :dev-dependencies [[leiningen/lein-swank "1.1.0"]])
+  :dev-dependencies [[swank-clojure "1.2.1"]])
---
+++ src/gml/temperature.clj
@@ -0,0 +1,33 @@
+(ns gml.temperature
+  (:require [gml.gml :as gml]))
+
+(defn- parse-double
+  [^String s]
+  (try
+    (Double/parseDouble s)
+    (catch NumberFormatException e 0)))
+
+(defn- f-to-c
+  [gui]
+  (let [f (parse-double (gml/get-property gui :f :text))
+	c (* 5/9 (- f 32))]
+    (dosync
+     (alter (gml/gui-model gui) assoc :f f :c c))))
+
+(defn- c-to-f
+  [gui]
+  (let [c (parse-double (gml/get-property gui :c :text))
+	f (+ 32 (* 9/5 c))]
+    (dosync
+     (alter (gml/gui-model gui) assoc :f f :c c))))
+
+(defn start
+  []
+  (gml/make-gui
+   (ref {:f 32 :c 0})
+   [:window [:title "Temp converter" :size [300 100]]
+    [:grid [:layout [:rows 2 :columns 2]]
+     [:label [:text "Fahrenheit"]]
+     [:label [:text "Celsius"]]
+     [:text [:text #(str (double (:f %))) :name :f :action f-to-c]]
+     [:text [:text #(str (double (:c %))) :name :c :action c-to-f]]]]))
